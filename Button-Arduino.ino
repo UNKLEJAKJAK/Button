dc37dcb783ca6b75d7b95cd24be559cdb72fffc5
franlew1776 <44408454+franlew1776@users.noreply.github.com>
2019-10-01 14:14:53 -0400

    Add files via upload

---
+++ Button-Arduino.ino
@@ -0,0 +1,36 @@
+/*
+  Buttin turns on/off led when pressed 
+
+  - LED attached from pin 13 to ground
+  - pushbutton attached to pin 2 from +5V
+  - 10K resistor attached to pin 2 from ground
+
+*/
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  14;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, INPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, OUTPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == LOW) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
